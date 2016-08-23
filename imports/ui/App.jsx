e01ce6f791595c3a4e95682cc8371b0a26457b81
meganmcgeee <meganpmcgee@gmail.com>
2016-08-23 13:22:15 -0700

    Added react to project. Created a ui folder. Created an app component that now renders via main.html. Added a form to app body.

---
+++ imports/ui/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// App component - represents the whole app
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>____'s Card</h1>
+        </header>
+
+        <ul>
+          <form className="new-card">
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Add the info you would like displayed on your card"
+            />
+          </form>
+        </ul>
+      </div>
+    );
+  }
+}
